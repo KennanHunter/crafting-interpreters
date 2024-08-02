2cab104482f8c195e52be9728e0ebe15c4ead2d5
Kennan <kennanhunter5@gmail.com>
2024-08-02 17:14:53 -0400

    style web interface a little bit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,37 @@
+import "./App.css";
 import { useEffect, useState } from "react";
 import init, { run } from "./wasm/rust-lox";
 import { useLogResults } from "./store";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useCodeState = create<{
+  code: string;
+  setCode: (newCode: string) => void;
+}>()(
+  persist(
+    (set) => ({
+      code: `
+let test_variable = "hello";
+
+print(test_variable + " world");
+
+print(5 + 3 * 2);
+`,
+      setCode: (newCode) => {
+        set({
+          code: newCode,
+        });
+      },
+    }),
+    {
+      name: "code-storage",
+    }
+  )
+);
 
 function App() {
-  const [code, setCode] = useState("");
+  const { code, setCode } = useCodeState();
   const [isLoaded, setIsLoaded] = useState(false);
 
   const { log, clear } = useLogResults();
@@ -15,30 +43,70 @@ function App() {
   });
 
   return (
-    <div>
-      <textarea value={code} onInput={(e) => setCode(e.currentTarget.value)} />
-      <br />
-      <button
-        disabled={!isLoaded}
-        onClick={() => {
-          run(code);
+    <div
+      style={{
+        display: "grid",
+        gap: "3.5em",
+        height: "100%",
+        width: "90%",
+        gridTemplateColumns: "1fr 1fr",
+        padding: "10px 5%",
+      }}
+    >
+      <textarea
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        value={code}
+        onInput={(e) => setCode(e.currentTarget.value)}
+      />
+
+      <div
+        style={{
+          display: "flex",
+          position: "absolute",
+          left: "0",
+          top: "0",
+          width: "100%",
+          justifyContent: "center",
         }}
       >
-        Run
-      </button>
-      <button
-        onClick={() => {
-          clear();
-        }}
-      >
-        Clear
-      </button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "0.5em",
+          }}
+        >
+          <button
+            disabled={!isLoaded}
+            onClick={() => {
+              run(code);
+            }}
+          >
+            Run
+          </button>
+          <button
+            onClick={() => {
+              clear();
+            }}
+          >
+            Clear
+          </button>
+        </div>
+      </div>
 
       <pre
         style={{
-          border: "2px solid black",
-          width: "15em",
-          height: "10em",
+          border: "1px solid black",
+          borderRadius: "2px",
+          width: "100%",
+          height: "100%",
+          margin: 0,
+          padding: 0,
+          whiteSpace: "pre-wrap",
+          overflowWrap: "anywhere",
         }}
       >
         {log.join("\n")}
