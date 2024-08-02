6e55c74c6c99efbee725e5f1f3b792e128d18705
Kennan <kennanhunter5@gmail.com>
2024-08-02 13:46:16 -0400

    Improve argument parsing

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,19 @@ fn main() {
 
     let args: Vec<String> = std::env::args().collect();
 
-    let file_name: Result<&str, &str> = match args.len() {
-        3.. => Err(""),
-        2 => {
-            let file_name = args.get(1).unwrap();
+    let file_name: Result<&str, &str> = match args.as_slice() {
+        [] => unreachable!("Will always have at least the path of the executable"),
 
+        // TODO: Use the run function to create a REPL
+        [_] => Ok("./demo.lox"),
+
+        [_, file_name] => {
             println!("Target file: {}", file_name);
 
             Ok(file_name.as_str())
         }
-        // TODO: Use the run function to create a REPL
-        1 => Ok("./demo.lox"),
-        0 => unreachable!("Will always have at least the path of the executable"),
+
+        _too_many => Err(""),
     };
 
     let source_file: String = read_file(file_name.unwrap());
